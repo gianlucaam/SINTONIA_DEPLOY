078667de7cc1f4b31e8d7ffdc2004f17d5f5c498
mattFanz <mattia.fanzini@gmail.com>
2025-11-27 16:59:26 +0100

    refactor: modifica reindirizzamento path frontend admin-dashboard.

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import AdminDashboardPage from './pages/AdminDashboardPage'
 import './App.css'
 
 function App() {
   return (
-    <AdminDashboardPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+        <Route path="/" element={<Navigate to="/admin-dashboard" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
