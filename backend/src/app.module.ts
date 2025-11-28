ad23dc97d2efc8dff45b7c2ae9909c9880979df9
AntonioWalterDeFusco <defuscoantoniowalter@gmail.com>
2025-11-29 00:56:59 +0100

    REFACTOR: restyling della selezione di uno psicologo nella modifica paziente

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,9 +9,10 @@ import { PatientModule } from './patient/patient.module.js';
 import { QuestionnairesModule } from './questionari/questionnaires.module.js';
 import { Questionari_amministratoreModule } from './amministratore/questionari/questionari_amministratore.module.js';
 import { Pazienti_amministratoreModule } from './amministratore/pazienti/pazienti_amministratore.module.js';
+import { Psicologi_amministratoreModule } from './amministratore/psicologi/psicologi_amministratore.module.js';
 
 @Module({
-  imports: [AuthModule, DashboardModule, SpidAuthModule, PatientModule, PsiModule, QuestionnairesModule, Questionari_amministratoreModule, Pazienti_amministratoreModule],
+  imports: [AuthModule, DashboardModule, SpidAuthModule, PatientModule, PsiModule, QuestionnairesModule, Questionari_amministratoreModule, Pazienti_amministratoreModule, Psicologi_amministratoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
