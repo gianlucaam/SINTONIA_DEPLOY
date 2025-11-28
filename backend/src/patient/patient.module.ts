fcdfd48267385af23540be71f9916c8042454fca
aleds25 <alessiods04@gmail.com>
2025-11-28 18:31:30 +0100

    refactor: rimossa importazione duplicata di ForumModule
    Eliminata l'importazione ripetuta di ForumModule per migliorare la pulizia del codice.

--- backend/src/patient/patient.module.ts
+++ backend/src/patient/patient.module.ts
@@ -4,10 +4,9 @@ import { HomeService } from './home/home.service.js';
 import { TermsController } from './terms/terms.controller.js';
 import { TermsService } from './terms/terms.service.js';
 import { ForumModule } from './forum/forum.module.js';
-
 import { SpidAuthModule } from '../spid-auth/spid-auth.module.js';
 import { QuestionarioModule } from './questionario/questionario.module.js';
-import { ForumModule } from './forum/forum.module.js';
+
 
 @Module({
     imports: [SpidAuthModule, QuestionarioModule, ForumModule],
